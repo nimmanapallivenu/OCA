ff0a2d170335a8c8fa2a8999b1d08fdac3fbd0ec
venu.nimmanapalli <venu.nimmanapalli@ASPIRENAV73.Aspiresys.com>
2017-04-10 17:54:22 +0530

    added Assessment question in chapter1

--- OCA/Chapter1/src/main/java/com/venu/ocjp/chapter1/Demo.java
+++
@@ -1,8 +0,0 @@
-package com.venu.ocjp.chapter1;
-
-public class Demo {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-}
---
+++ OCA/Chapter1/src/main/java/com/venu/ocjp/chapter1/_C.java
@@ -0,0 +1,24 @@
+package com.venu.ocjp.chapter1;
+
+/**
+ * Assessment Test Question
+ * 
+ * @author venu.nimmanapalli
+ *
+ */
+public class _C {
+	private static int $;
+
+	/**
+	 * @param main Output 
+	 *<br>
+	 * 0null
+	 */
+	public static void main(String[] main) {
+		String a_b = null; // Actual code is String a_b; we will get compilation error here local variable must be initialized 
+		System.out.print($);
+		System.out.print(a_b);
+	}
+	
+	
+}
